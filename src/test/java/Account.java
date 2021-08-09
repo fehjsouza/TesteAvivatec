c93a9338015b577c2adca6bad445a4cd022d3af0
Fehjsouza <feh_jsouza@hotmail.com>
2021-08-09 12:47:50 -0300

    added the selenium config

--- src/test/java/Account.java
+++ src/test/java/Account.java
@@ -1,11 +1,34 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Account {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupWebdriverChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
+    }
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
     @Given("^the user navigate to register page")
     public void givenStatement() {
-        System.out.println("Given statement executed successfully");
+        driver.get("https://www.github.com");
     }
 
     @When("^the user fill the fields")

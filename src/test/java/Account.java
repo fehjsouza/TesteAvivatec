543301ae927245097333d871eba62a5d818ab29d
Fehjsouza <feh_jsouza@hotmail.com>
2021-08-09 14:03:53 -0300

    first test complete

--- src/test/java/Account.java
+++ src/test/java/Account.java
@@ -1,22 +1,28 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.graalvm.compiler.debug.Assertions;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.UUID;
 
 public class Account {
 
-    private WebDriver driver;
+    WebDriver driver;
 
-    @BeforeClass
-    public static void setupWebdriverChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
-    }
     @Before
-    public void setup() {
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
+                + "/src/test/resources/chromedriver");
+
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+
         driver = new ChromeDriver();
     }
 
@@ -26,9 +32,28 @@ public class Account {
             driver.quit();
         }
     }
+
     @Given("^the user navigate to register page")
     public void givenStatement() {
-        driver.get("https://www.github.com");
+
+        driver.get("http://seubarriga.wcaquino.me/cadastro");
+        WebElement inputName = driver.findElement(By.id("nome"));
+        inputName.sendKeys("Luiz");
+
+        WebElement inputEmail = driver.findElement(By.id("email"));
+        inputEmail.sendKeys("Luiz@souza"+ UUID.randomUUID());
+
+        WebElement inputSenha = driver.findElement(By.id("senha"));
+        inputSenha.sendKeys("Felipe123");
+
+        WebElement btnCadastrar = driver.findElement(By.xpath("/html/body/div[2]/form/input"));
+        btnCadastrar.click();
+
+        WebElement msgSuccess = driver.findElement(By.xpath("/html/body/div[1]"));
+        String result = msgSuccess.getText();
+
+        Assert.assertEquals("Usuário inserido com sucesso", result);
+
     }
 
     @When("^the user fill the fields")
